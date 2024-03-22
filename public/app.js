c72b7cb286588a2b5d188c2d9e8c9a0142fb0ac1
musman-js <usman2037966@gmail.com>
2024-03-22 20:15:30 +0500

    My Personal Portfolio

---
+++ public/app.js
@@ -0,0 +1,116 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCZU3lD7Y6BuRF9iJu7H4POOZOM0H0B8OM",
+  authDomain: "musmanjs.firebaseapp.com",
+  databaseURL: "https://musmanjs-default-rtdb.firebaseio.com",
+  projectId: "musmanjs",
+  storageBucket: "musmanjs.appspot.com",
+  messagingSenderId: "371778779055",
+  appId: "1:371778779055:web:4609d0afcf42153da3a15a"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+
+let getValue = () => {
+  const firstName = document.getElementById('firstName');
+  const lastName = document.getElementById('lastName');
+  const email = document.getElementById('email');
+  const message = document.getElementById('message');
+  const phoneNumber = document.getElementById('phoneNumber');
+
+  if (firstName.value === "") {
+    alert('Please write name');
+  }
+
+  else if (email.value === "") {
+    alert('Please write email');
+  }
+
+  else if(phoneNumber.value === ""){
+    alert("Please write phone Number");
+  }
+
+  else if (message.value === "") {
+    alert('Please enter message')
+  }
+
+  else {
+    let obj = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      phoneNumber: phoneNumber.value,
+      email: email.value,
+      message: message.value
+    }
+
+
+    Swal.fire({
+      title: "Your Message has Sent!",
+      text: "I will response you soon.",
+      showClass: {
+        popup: `
+                animate__animated
+                animate__fadeInUp
+                animate__faster
+              `
+      },
+      hideClass: {
+        popup: `
+                animate__animated
+                animate__fadeOutDown
+                animate__faster
+              `
+      }
+    });
+
+    firebase.database().ref('user-information').push(obj);
+    firstName.value = "";
+    lastName.value = "";
+    phoneNumber.value = "";
+    email.value = "";
+    message.value = "";
+  }
+}
+
+
+
+//Preloader
+document.addEventListener("DOMContentLoaded", function() {
+  var loaderContainer = document.getElementById("loaderContainer");
+
+  setTimeout(function() {
+      loaderContainer.style.display = "none"; // hide loader container after 1 second
+  }, 1500);
+});
+
+
+// Toggle function
+document.addEventListener("DOMContentLoaded", function () {
+  function toggleOffcanvas() {
+    document.getElementById('toggleButton').click();
+  }
+
+  function attachClickListener() {
+    var navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(function (navLink) {
+      navLink.addEventListener('click', toggleOffcanvas);
+    });
+  }
+
+  if (window.innerWidth <= 991) {
+    attachClickListener();
+  }
+
+  window.addEventListener('resize', function () {
+    if (window.innerWidth <= 991) {
+      attachClickListener();
+    } else {
+      var navLinks = document.querySelectorAll('.nav-link');
+      navLinks.forEach(function (navLink) {
+        navLink.removeEventListener('click', toggleOffcanvas);
+      });
+    }
+  });
+});
+
